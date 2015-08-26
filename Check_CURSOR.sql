57e9ea76c040f87eb418594d82d59e3b32d14b98
hoanhtnb <hoanghoan91@gmail.com>
2015-08-26 09:50:04 +0700

    Check CURSOR
    Kiểm tra CURSOR có lấy được dữ liệu không, nếu không thì trả về 1 bản
    ghi

---
+++ Check_CURSOR.sql
@@ -0,0 +1,23 @@
+/*
+//=======================================================
+//https://github.com/hoanhtnb
+//Twitter: @hoangtronghoan
+//Facebook: facebook.com/ht.hoan
+
+--Muc dich de kiem tra neu CURSOR khong co du lieu ta se tra ve 1 dong trang
+--Rat nhieu truong hop ta can phai lam the
+--Bla bla
+//=======================================================   
+*/    
+IF (PV_REFCURSOR IS NULL) THEN
+BEGIN
+    OPEN pv_refcursor 
+    FOR
+        SELECT   ''id,'' name,0 total FROM   DUAL;
+EXCEPTION
+    WHEN OTHERS
+    THEN
+        RETURN;
+END;
+
+END IF;
